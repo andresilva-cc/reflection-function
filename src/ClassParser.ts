ef234808e65a7a9443acffcce4c68bda795a1bd8
André Luiz <andreluiz.97@outlook.com>
2021-10-03 19:22:20 -0300

    feat: Add ReflectionClass

---
+++ src/ClassParser.ts
@@ -0,0 +1,30 @@
+import Parser from './Parser';
+import ReflectionFunction from './ReflectionFunction';
+
+class ClassParser extends Parser {
+  public static override parse(classObject: Function): any {
+    const signature = this.parseSignature(classObject.toString());
+
+    if (!signature) {
+      throw new Error('Invalid object');
+    }
+
+    return {
+      name: this.parseName(signature),
+      classConstructor: new ReflectionFunction(classObject),
+      methods: this.parseMethods(classObject.prototype),
+    };
+  }
+
+  private static parseMethods(methods: object) {
+    const parsedMethods: Array<ReflectionFunction> = [];
+
+    Object.entries(methods).forEach(([key, value]) => {
+      parsedMethods.push(new ReflectionFunction(value, key));
+    });
+
+    return parsedMethods;
+  }
+}
+
+export default ClassParser;
---
+++ src/ReflectionClass.ts
@@ -0,0 +1,20 @@
+import ClassParser from './ClassParser';
+import ReflectionFunction from './ReflectionFunction';
+
+class ReflectionClass {
+  public readonly name: string;
+
+  public readonly classConstructor: ReflectionFunction;
+
+  public readonly methods: Array<ReflectionFunction>;
+
+  constructor(classReference: Function) {
+    const parsedClass = ClassParser.parse(classReference);
+
+    this.name = parsedClass.name;
+    this.classConstructor = parsedClass.classConstructor;
+    this.methods = parsedClass.methods;
+  }
+}
+
+export default ReflectionClass;
