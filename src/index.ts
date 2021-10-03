2a14003d4dfb457672a8bfd77b13e598ee9c6ee6
André Luiz <andreluiz.97@outlook.com>
2021-10-03 19:58:50 -0300

    fix: Add missing export for ReflectionClass in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
-/* eslint-disable import/prefer-default-export */
-
 export { default as ReflectionFunction } from './ReflectionFunction';
+export { default as ReflectionClass } from './ReflectionClass';
